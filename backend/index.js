92576ff74905c8a0c3710238036b39be25d334de
Palashpunde2003 <palashpunde2003@gmail.com>
2025-12-06 19:34:53 +0530

    Fix slow startup

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
